572b908cc42ea138719fbb3c08a49be9376f2972
_temp <78365254+solf5978@users.noreply.github.com>
2023-11-13 20:41:46 +0800

    modified the TO_SEND_MSG

--- tx_rx_non_blocking/main.c
+++ tx_rx_non_blocking/main.c
@@ -57,7 +57,7 @@
 
 /** @brief Symbol specifying pin number of master SCL. */
 #define MASTER_SCL_PIN LOOPBACK_PIN_4A
-
+#define TWI_ADDRESSES  127
 /** @brief Symbol specifying pin number of master SDA. */
 #define MASTER_SDA_PIN LOOPBACK_PIN_4B
 
@@ -177,6 +177,25 @@ static void twis_handler(nrfx_twis_evt_t const * p_event)
     }
 }
 
+void twi_init (void)
+{
+    ret_code_t err_code;
+
+    const nrf_drv_twi_config_t twi_config = {
+       .scl                = 26,
+       .sda                = 27,
+       .frequency          = NRF_TWI_FREQ_100K,
+       .interrupt_priority = APP_IRQ_PRIORITY_HIGH,
+       .clear_bus_init     = false
+    };
+
+    err_code = nrf_drv_twi_init(&m_twi, &twi_config, NULL, NULL);
+    APP_ERROR_CHECK(err_code);
+
+    nrf_drv_twi_enable(&m_twi);
+}
+
+
 /**
  * @brief Function for application main entry.
  *
@@ -188,6 +207,36 @@ int main(void)
     (void)status;
 
     NRFX_EXAMPLE_LOG_INIT();
+
+    ret_code_t err_code;
+    uint8_t address;
+    uint8_t sample_data;
+    bool detected_device = false;
+
+    APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+
+    NRF_LOG_INFO("TWI scanner.");
+    NRF_LOG_FLUSH();
+    twi_init();
+
+    for (address = 1; address <= TWI_ADDRESSES; address++)
+    {
+        err_code = nrf_drv_twi_rx(&m_twis_inst, address, &sample_data, sizeof(sample_data));
+        if (err_code == NRFX_SUCCESS)
+        {
+            detected_device = true;
+            NRF_LOG_INFO("TWI device detected at address 0x%x.", address);
+        }
+        NRF_LOG_FLUSH();
+    }
+
+    if (!detected_device)
+    {
+        NRF_LOG_INFO("No device was found.");
+        NRF_LOG_FLUSH();
+    }
+
 
     NRFX_LOG_INFO("Starting nrfx_twim_twis non-blocking example.");
     NRFX_EXAMPLE_LOG_PROCESS();
