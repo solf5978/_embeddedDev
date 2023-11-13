ce0fb9260980b86b5bea59e62a6e037c045ed410
_temp <78365254+solf5978@users.noreply.github.com>
2023-11-13 21:02:58 +0800

    remove redundant files and add one impl

---
+++ ncs_impl/initial/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "nrf52840dk_nrf52840" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "D:/_readilyMGMT/_embeddedDev/ncs_impl/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "D:/_readilyMGMT/_embeddedDev/_nRF_SDK/toolchains/c57af46cb7/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
